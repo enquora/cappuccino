3127f3a8688ea6564cd463350f3d774ab42a6b43
David Richardson <david.richardson@enquora.com>
2024-01-03 11:18:09 -0700

    Reorganize code in Xcode Project Navigator Organization code in functional groups rather than by type. Improves understanding of code functionality in absence of other visual or textual documentation.

---
+++ Tools/XcodeCapp/XcodeCapp/Application Settings/XCCApplicationSettingsController.h
@@ -0,0 +1,17 @@
+//
+//  XCCApplicationSettingsController.h
+//  XcodeCapp
+//
+//  Created by David Richardson on 2024-01-03.
+//  Copyright © 2024 cappuccino.dev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCCApplicationSettingsController : NSViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tools/XcodeCapp/XcodeCapp/Application Settings/XCCApplicationSettingsController.m
@@ -0,0 +1,22 @@
+//
+//  XCCApplicationSettingsController.m
+//  XcodeCapp
+//
+//  Created by David Richardson on 2024-01-03.
+//  Copyright © 2024 cappuccino.dev. All rights reserved.
+//
+
+#import "XCCApplicationSettingsController.h"
+
+@interface XCCApplicationSettingsController ()
+
+@end
+
+@implementation XCCApplicationSettingsController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
