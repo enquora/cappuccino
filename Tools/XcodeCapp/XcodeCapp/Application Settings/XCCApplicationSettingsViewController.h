5a35215673ecd7274477fc72f4841f0f8796b7c8
David Richardson <david.richardson@enquora.com>
2025-08-25 14:34:52 -0600

    Add controller and xib for application settings
    Non-functional placeholder only.
    Xib contains copy of existing Preferences window from MainMenu.xib.

---
+++ Tools/XcodeCapp/XcodeCapp/Application Settings/XCCApplicationSettingsViewController.h
@@ -0,0 +1,17 @@
+//
+//  XCCApplicationSettingsViewController.h
+//  XcodeCapp
+//
+//  Created by David Richardson on 2025-08-25.
+//  Copyright © 2025 cappuccino-project. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCCApplicationSettingsViewController : NSViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tools/XcodeCapp/XcodeCapp/Application Settings/XCCApplicationSettingsViewController.m
@@ -0,0 +1,22 @@
+//
+//  XCCApplicationSettingsViewController.m
+//  XcodeCapp
+//
+//  Created by David Richardson on 2025-08-25.
+//  Copyright © 2025 cappuccino-project. All rights reserved.
+//
+
+#import "XCCApplicationSettingsViewController.h"
+
+@interface XCCApplicationSettingsViewController ()
+
+@end
+
+@implementation XCCApplicationSettingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
